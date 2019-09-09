79f7e8d87f89a7ec6d53cc8f99f01bf50571e455
ricardomayerb <ricardomayerb@gmail.com>
2019-09-09 07:00:55 -0300

    foo

---
+++ clase_09_sept.R
@@ -0,0 +1,21 @@
+# General idea: evaluate forecast accuracy:
+
+# First example from the book
+# Second, two series from BCCh
+
+# Third, download a financial series using quantmod
+
+# Review the measures and program by hand a hardcoded evaluation of
+# Naive or season naive or trend naive against an auto.arima
+# Introduce seasonal arima notation
+
+# Then introduce the forecast package functions for accuracy
+
+
+# Fourth, introduce time series cross validation
+
+
+
+
+
+
